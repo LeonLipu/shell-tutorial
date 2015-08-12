2c926b32bb099c753887d9ccec9b1043e4900daf
leonlipu <lipusipupara@gmail.com>
2015-08-12 05:39:22 +0530

    this is about command line argument to shell script

---
+++ day3.sh
@@ -0,0 +1,18 @@
+#this is about reciving parameter to shell scripting
+
+echo $# #this number of argument
+echo $@ # treat argument separated by space
+echo $* #treat argument separated by space
+echo "$@" # treat argument separated by quote ""
+echo $? # check previous statement is excuted successful
+
+echo "argument "
+for arg in $@ ; do
+echo ${arg}
+done
+
+
+echo "quote is delimeter to separate argument   ./sh 'what is problem'"
+for arg in "$@" ; do
+  echo ${arg}
+done
