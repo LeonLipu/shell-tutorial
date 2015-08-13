9663d778f36365832b9784515b97d911bff4f343
leonlipu <lipusipupara@gmail.com>
2015-08-13 23:51:03 +0530

    few more command in sed

---
+++ day4.sh
@@ -0,0 +1,29 @@
+cat forsed.txt
+echo "1.________________________________________"
+
+#only first occurance of file
+sed 's/unix/linux/' forsed.txt
+
+echo "2.________________________________________"
+
+#only 2nd occurance
+sed 's/unix/linux/2' forsed.txt
+echo "3.________________________________________"
+
+#global occurance
+sed 's/unix/linux/g' forsed.txt
+
+#sed 'operation /regularexpression/replacestring/action'
+echo "4.________________________________________"
+#replacd matched line
+sed '/linux/ s/unix/centos/' forsed.txt
+
+echo "5.________________________________________"
+#delete line
+sed '2,$ d' forsed.txt
+
+echo "6.________________________________________"
+#add line after a befor i and changeline c
+sed '/unix/ a "lipu"' forsed.txt
+
+#sed 'p' will duplicate lines and sed -n 'p' only one line and sed -n '/unix/ p' is same as grep command !p is opposite
