a40b47faf2aa46b01b98a5a7e5855c29bb9bb441
leonlipu <lipusipupara@gmail.com>
2015-08-21 23:39:20 +0530

    head and awk basic

---
+++ headandtail.sh
@@ -0,0 +1,17 @@
+
+echo "***********get 10 line *********"
+
+head head.txt
+
+echo "***********get n line****************************"
+head -n7 head.txt|sort
+
+echo "**********except last 6*******************"
+head -n-6 head.txt
+echo "***********3 th line in every line *******************"
+
+
+
+awk '{print $3}' head.txt
+
+awk '{print length($2)}' head.txt
