76c01cfa78dcdab59dcf27cd1de6f63c1c9da30a
leonlipu <lipusipupara@gmail.com>
2015-08-27 04:11:29 +0530

    switch case and condition check with regular expression

--- day1.sh
+++ day1.sh
@@ -7,3 +7,28 @@ done
 if [ $# -eq 0 ]; then
   echo "this is if Bconsidion"
 fi
+
+day="sunday"
+case "$day" in
+  "sunday" )
+    echo "today is my first day"
+    ;;
+    "monday" )
+    echo "today is second day "
+    ;;
+    "tuesday")
+    echo "today is third day"
+    ;;
+    "friday")
+    echo "today is weekends"
+    ;;
+
+esac
+
+name="brahmanandaaaaa"
+echo "$name"
+
+if [[ "$name" =~ brahmananda* ]]
+  then
+  echo "it is matched "
+fi
