50f80b291c373d34245ead00dd34e53ab251080c
LeonLipu <lipusipupara@gmail.com>
2015-10-20 23:39:12 +0530

    uname command been added

---
+++ uname.sh
@@ -0,0 +1,24 @@
+#give karnel  name
+echo "*********uname********"
+uname #linux
+echo "*************uname -n****"
+#get network host name
+uname -n
+echo "*********uname -v ************"
+#karnel version
+uname -v
+
+echo "********* uname -r ***********"
+#get release of karnel
+uname -r
+
+echo "**************uname -m ***************"
+#get machine hardware
+uname -m
+echo "***************uname -a *************"
+#get all information of system
+uname -a
+
+echo "*************** sudo lshw************"
+#get all info about hardware  to this command you need to have sudo permisson 
+sudo lshw
