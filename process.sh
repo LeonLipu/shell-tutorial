478fe55ebfa4c167bb00009085fda9b2f4949aab
LeonLipu <lipusipupara@gmail.com>
2015-10-30 23:25:42 +0530

    process command in shell programming

---
+++ process.sh
@@ -0,0 +1,37 @@
+
+#list of all processes
+ps -A
+
+ps -A|grep "atom"
+
+#tradional way to see system uses
+top
+
+#processes to see visiualisation way
+
+pstree
+
+
+#end the processes by id
+kill pid
+
+#getting pid of a process
+pgrep atom
+
+#the kill that process
+kill $(pgrep atom)
+
+#kill by process name
+killall atom
+
+#kill by process name
+pkill atom
+
+
+#how to change priority of process -19 is very high and +19 is very low and 0 is default
+
+renice 19 pid
+
+#kill any process by moving mouse click
+
+xkill 
