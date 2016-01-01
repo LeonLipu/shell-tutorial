fb49892955d0e12af138a8ce21c573f4c95cae91
LeonLipu <lipusipupara@gmail.com>
2016-01-01 23:22:38 +0530

    Create tar.sh
    tar command help c x t crating extracting and view ing vf verbose and following and z gz and j for bz2

---
+++ tar.sh
@@ -0,0 +1,19 @@
+# explanation for tar command
+1.creating tar file
+
+tar cvf name.tar directoryname
+
+2.extracting from tar file
+
+tar xvf name.tar
+
+3.view tar file
+
+tar tvf name.tar file
+
+note : c -creating x-extracting t-view
+
+cvf =creating verbosely following archive file
+
+cvfz =z for gzip file with extension .gz
+cvfj =j for bzip file with extension .bz2
